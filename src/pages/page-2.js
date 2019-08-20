945f16e6cf6ba6d585f59d69bf0f214adfaeb72c
Francisca Sanchez <mfransanchez@gmail.com>
2019-08-20 15:59:08 -0400

    primer cambio

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <iframe title="Reel 2018" src="https://player.vimeo.com/video/250702471" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
   </Layout>
 )
 
